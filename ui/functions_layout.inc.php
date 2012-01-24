2d74455e7ef4252557d2d71de8364c9655db6358
Sven Wagener <svenw@themekraft.com>
2012-01-24 17:01:30 +0100

    Changed CSS path

--- ui/functions_layout.inc.php
+++ ui/functions_layout.inc.php
@@ -6,7 +6,7 @@ if(!function_exists('alert')){
 	}
 }
 function defblog_css(){
-	$defblog_plugin_path=dirname(__FILE__);
+	global $defblog_plugin_path;
 	echo "<link rel=\"stylesheet\" href=\"".get_option('siteurl')."/wp-content/plugins/".basename($defblog_plugin_path)."/ui/styles.css\" type=\"text/css\" media=\"screen\" />";
 }
 if(!function_exists('ajaxui_js')){
